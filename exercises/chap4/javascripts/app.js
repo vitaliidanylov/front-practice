53c2ece97f3efe5d21178e6f3796a7fab041beae
Vitalii Danylov <sincler2013@gmail.com>
2016-02-04 01:27:23 +0200

    finished practice for chapter4

---
+++ exercises/chap4/javascripts/app.js
@@ -0,0 +1,6 @@
+var main = function(){
+	"use stict"
+	alert("VALERA!!!");
+};
+
+$(document).ready(main);
---
+++ exercises/chap4/javascripts/appSelc.js
@@ -0,0 +1,13 @@
+var main = function () {
+"use strict;"
+// $("*").css("color","red");
+// $(".important").css("color","red");
+// $(".relevant p:first-child").css("color","red");
+// $(".relevant p:nth-child(3)").css("color","red");
+// $(".relevant p").css("color","red");
+// $(".relevant p:nth-child(even)").css("color","red");
+// $(".relevant p:nth-child(7)").css("color","red");
+// $(".relevant p:gt(3)").css("color","red");
+// $(".relevant p:not(.a)").css("color","red");
+};
+$(document).ready(main);
